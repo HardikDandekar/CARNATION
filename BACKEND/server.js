c87b3fac8f59a076561170fc91d31b21750d8788
HardikDandekar <hardikdandekar35@gmail.com>
2025-04-26 16:12:48 +0530

    server,js changed

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -23,8 +23,6 @@ app.use(cors(corsOptions));
 
 // MongoDB Connect
 mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
 })
   .then(() => console.log("✅ MongoDB connected"))
   .catch((err) => console.log("❌ MongoDB connection error:", err));
